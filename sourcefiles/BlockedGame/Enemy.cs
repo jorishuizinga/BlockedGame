f1c3316dc951298d37c89cd30257b91f355735da
Joris <jorishuizinga@me.com>
2017-10-20 01:56:32 +0200

    Prepared enemy class

--- sourcefiles/BlockedGame/Enemy.cs
+++ sourcefiles/BlockedGame/Enemy.cs
@@ -1,5 +1,11 @@
 ﻿namespace GXPEngine{
-    public class Enemy{
-        
+    public class Enemy : AnimationSprite{
+        public Enemy() : base("enemy.png", 8, 1, 8){
+            
+        }
+
+        public void Update(){
+            
+        }
     }
 }
