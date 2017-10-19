d6647cbec343af8276bdab9a05b34c5c70005e3a
Joris <jorishuizinga@me.com>
2017-10-20 01:40:33 +0200

    Implemented Timer class

--- sourcefiles/BlockedGame/Timer.cs
+++ sourcefiles/BlockedGame/Timer.cs
@@ -1,5 +1,20 @@
-﻿namespace GXPEngine{
-    public class Timer{
-        
+﻿using System;
+
+namespace GXPEngine{
+    public class Timer : GameObject{
+        private int _time;
+        private Action _timerAction;
+
+        public Timer(int time, Action timerAction){
+            this._time = time;
+            this._timerAction = timerAction;
+        }
+
+        public void Update(){
+            _time -= Time.deltaTime;
+            if (_time > 0) return;
+            _timerAction();
+            Destroy();
+        }
     }
 }
