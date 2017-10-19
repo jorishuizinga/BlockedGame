60d07590c8633b8352e21dc476d6674f1afd9218
Joris <jorishuizinga@me.com>
2017-10-20 01:55:49 +0200

    Updated Player class

--- sourcefiles/BlockedGame/Player.cs
+++ sourcefiles/BlockedGame/Player.cs
@@ -2,15 +2,18 @@
 
 namespace GXPEngine{
     public class Player : AnimationSprite{
-        private float playerSpeed = 10;
+        private float playerSpeed = 3;
+        private int frameCounter;
         
-        public Player() : base("colors.png", 1, 1, 1){
+        public Player() : base("player.png", 8, 1, 8){
             SetOrigin(width / 2, height / 2);
             y = game.height / 2;
             x = game.width / 2;
+            frameCounter = 0;
         }
 
         public void Update(){
+            //Input
             if (Input.GetKey(Key.W)){
                 y -= playerSpeed;
             }
@@ -23,6 +26,13 @@ namespace GXPEngine{
             if (Input.GetKey(Key.D)){
                 x += playerSpeed;
             }
+            
+            //Animation
+            if (frameCounter >= 20){
+                NextFrame();
+                frameCounter = 0;
+            }
+            frameCounter++;
         }
     }
 }
