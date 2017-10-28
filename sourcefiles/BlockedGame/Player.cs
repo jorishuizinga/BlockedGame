3058f38dd092d18058fa6d2b39ebe9a215405387
Joris <jorishuizinga@me.com>
2017-10-28 14:57:14 +0200

    Worked on player movement

--- sourcefiles/BlockedGame/Player.cs
+++ sourcefiles/BlockedGame/Player.cs
@@ -3,74 +3,126 @@ using System.Drawing;
 
 namespace GXPEngine{
     public class Player : AnimationSprite{
-        private float playerSpeed = 3;
-        private float xSpeed;
-        private float ySpeed;
-        private int frameCounter;
-        private int idleCounter;
+        private float _xSpeed;
+        private float _ySpeed;
+        private const float JumpForce = 4;
+        private float _initialJumpY;
+        private const float JumpHeight = 250;
+        private int _frameCounter;
+        private int _idleCounter;
+        private bool _jumpStarted;
 
-        private Level level;
+        private readonly Level _level;
 
-        enum PlayerState{
-            IDLE,
-            WALKING,
-            SHOOTING,
-            JUMPING,
-            FALLING
+        private enum PlayerState{
+            Idle,
+            Walking,
+            Shooting,
+            Jumping,
+            Falling
         };
 
-        enum JumpState{
-            NONE,
-            ASCENDING,
-            DESCENDING
+        private enum JumpState{
+            None,
+            Ascending,
+            Descending
         }
 
-        private PlayerState playerState;
+        private PlayerState _playerState;
+        private JumpState _jumpState;
         
         public Player(Level level) : base("player.png", 8, 1, 8){
-            this.level = level;
-            SetOrigin(width / 2, height / 2);
+            this._level = level;
+            // ReSharper disable once PossibleLossOfFraction
+            SetOrigin(width / 2, 0);
+            // ReSharper disable once PossibleLossOfFraction
             y = game.height / 2;
+            // ReSharper disable once PossibleLossOfFraction
             x = game.width / 2;
-            frameCounter = 0;
+            _playerState = PlayerState.Falling;
+            _jumpStarted = false;
+            _frameCounter = 0;
+        }
+
+        //Seal height and width variables to prevent class from virtually accessing variables from AnimationSprite class
+        public sealed override int height{
+            get{ return base.height; }
+            set{ base.height = value; }
+        }
+
+        public sealed override int width{
+            get{ return base.width; }
+            set{ base.width = value; }
         }
 
         public void Update(){
             //Input
-            if (Input.GetKey(Key.LEFT)){
-                xSpeed -= 2;
-                playerState = PlayerState.WALKING;
-            }else if (Input.GetKey(Key.RIGHT)){
-                xSpeed += 2;
-                playerState = PlayerState.WALKING;
-            }else if (Input.GetKey(Key.UP) && (playerState == PlayerState.IDLE || playerState == PlayerState.WALKING)){
-                playerState = PlayerState.JUMPING;
-            }else if (idleCounter >= 20 && playerState != PlayerState.IDLE){
-                playerState = PlayerState.IDLE;
+            //TODO: Clean up the following code
+            //!-->
+            if (Input.GetKey(Key.LEFT) && _playerState != PlayerState.Falling){
+                _xSpeed -= 1;
+                if(_playerState != PlayerState.Jumping) _playerState = PlayerState.Walking;
+            }else if (Input.GetKey(Key.RIGHT) && _playerState != PlayerState.Falling){
+                _xSpeed += 1;
+                if(_playerState != PlayerState.Jumping) _playerState = PlayerState.Walking;
+            }else if (Input.GetKey(Key.UP) && (_playerState == PlayerState.Idle || _playerState == PlayerState.Walking) && !_jumpStarted){
+                _playerState = PlayerState.Jumping;
+            }else if (_idleCounter >= 20 && _playerState != PlayerState.Idle && _playerState != PlayerState.Jumping && _playerState != PlayerState.Falling){
+                _playerState = PlayerState.Idle;
             }else{
-                idleCounter++;
+                _idleCounter++;
             }
 
-            if (playerState == PlayerState.FALLING){
-                
+            if (_playerState == PlayerState.Falling){
+                _ySpeed += 1;
+            }else if(_playerState == PlayerState.Idle && _ySpeed != 0){
+                _ySpeed = 0;
+                y = _level.GetFloor(this);
+            }else if (_playerState == PlayerState.Jumping){
+                if (!_jumpStarted){
+                    _jumpState = JumpState.Ascending;
+                    _jumpStarted = true;
+                    _initialJumpY = y;
+                }else if (_jumpState == JumpState.Ascending){
+                    if(!(y <= _initialJumpY - JumpHeight)){
+                        y -= JumpForce;
+                    }else{
+                        _jumpState = JumpState.Descending;
+                    }
+                }else if (_jumpState == JumpState.Descending){
+                    _playerState = PlayerState.Falling;
+                    _jumpState = JumpState.None;
+                    _jumpStarted = false;
+                }
             }
 
-            x += xSpeed;
-            level.x -= xSpeed;
+            if (y > _level.GetFloor(this) && (_playerState == PlayerState.Falling || _playerState == PlayerState.Jumping)){
+                _playerState = PlayerState.Idle;
+            }
+            //<--!
 
-            xSpeed *= 0.9f;
+            x += _xSpeed;
+            y += _ySpeed;
+            _level.x -= _xSpeed;
+
+            _xSpeed *= 0.9f;
+            _ySpeed *= 0.9f;
             
             //Animation
-            if (playerState == PlayerState.WALKING){
-                if (frameCounter >= 15){
+            if (_playerState == PlayerState.Walking){
+                if (_frameCounter >= 15){
                     NextFrame();
-                    frameCounter = 0;
+                    _frameCounter = 0;
                 }
-                frameCounter++;
+                _frameCounter++;
             }else{
                 currentFrame = 0;
-                frameCounter = 0;
+                _frameCounter = 0;
             }
+            
+            //Temporary debug console output
+            // ReSharper disable once HeapView.BoxingAllocation
+            Console.WriteLine(_playerState);
         }
     }
 }
